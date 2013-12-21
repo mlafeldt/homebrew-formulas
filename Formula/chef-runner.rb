cbf28d5e7f24051ce6bfbde4a3fd92eaf7ee09f2
fh <fh-github@fholzhauer.de>
2013-12-21 13:22:58 +0100

    Added Readme and shasum for zip

--- Formula/chef-runner.rb
+++ Formula/chef-runner.rb
@@ -4,6 +4,7 @@ class ChefRunner < Formula
   homepage 'https://github.com/mlafeldt/chef-runner'
   url 'https://github.com/mlafeldt/chef-runner/archive/v0.1.1.zip'
   head 'https://github.com/mlafeldt/chef-runner.git'
+  sha1 'b65a88f86bf90558c6b01fee1ff361f9101bc034'
 
   def install
     bin.install Dir['bin/chef-runner']
