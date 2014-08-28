99791f2200dcb1a6d890bae844897889ecaacafa
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-28 15:33:03 +0200

    Update chef-runner formula to install v0.6.0

--- Formula/chef-runner.rb
+++ Formula/chef-runner.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class ChefRunner < Formula
   homepage "https://github.com/mlafeldt/chef-runner"
-  version "0.5.0"
+  version "0.6.0"
 
   if Hardware.is_64_bit?
     url "https://github.com/mlafeldt/chef-runner/releases/download/v#{version}/chef-runner_#{version}_darwin_amd64.zip"
-    sha256 "3b6b74d44fa3f3fc62dd48caf14ce2ac251880935d4ba86966ca7e068668586f"
+    sha256 "c23103b1b43107e31d682751d71b40991d89cb3482a79c35e45017759471babc"
   else
     url "https://github.com/mlafeldt/chef-runner/releases/download/v#{version}/chef-runner_#{version}_darwin_386.zip"
-    sha256 "df6c3fab33f761a8f44760e76bdd07aec6b0322a7dde48ba85b7ee239225ffb7"
+    sha256 "c10750901c1c5ae2c31d83674d81e38036931ba5884f572d88757d2ed25be8f2"
   end
 
   def install
