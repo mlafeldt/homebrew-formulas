52ab5068b4a53847e0673f5343ffcf16033d5fa5
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2016-07-15 13:21:24 +0200

    Update chaosmonkey to v0.4.0
    https://github.com/mlafeldt/chaosmonkey/releases/tag/v0.4.0

--- Formula/chaosmonkey.rb
+++ Formula/chaosmonkey.rb
@@ -1,9 +1,9 @@
 class Chaosmonkey < Formula
   desc     "Go client to the Chaos Monkey REST API"
   homepage "https://github.com/mlafeldt/chaosmonkey"
-  version  "0.3.0"
+  version  "0.4.0"
   url      "https://github.com/mlafeldt/chaosmonkey/releases/download/v#{version}/chaosmonkey_darwin_amd64"
-  sha256   "8578533c978689570484dd178184bfb9a136ea86672646b8c4418619b5967f3e"
+  sha256   "e9c3a167b470341b7b867d6424df85515310cc9d32cb81e9fe5fa725384f1de5"
 
   bottle :unneeded
 
