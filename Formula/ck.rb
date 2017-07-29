6e950996a079d72228ad7f69287fb76e3c4a8b23
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2017-07-29 14:46:06 +0200

    Add recipe for ck

---
+++ Formula/ck.rb
@@ -0,0 +1,17 @@
+class Ck < Formula
+  desc     "The ConvertKit Tool"
+  homepage "https://github.com/mlafeldt/ck"
+  version  "0.1.0"
+  url      "https://github.com/mlafeldt/ck/releases/download/v#{version}/ck_darwin_amd64"
+  sha256   "461ce11f5a7ead74fb518e7c37bf859429d55180f1610e5ba47cd23fa507fd09"
+
+  bottle :unneeded
+
+  def install
+    bin.install "ck_darwin_amd64" => "ck"
+  end
+
+  test do
+    system "#{bin}/ck --version"
+  end
+end
