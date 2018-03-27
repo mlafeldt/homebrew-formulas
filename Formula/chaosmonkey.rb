da05a393d556520e98f0268d324a3d6e6cacd60b
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-03-27 15:26:31 +0200

    Update chaosmonkey to latest release

--- Formula/chaosmonkey.rb
+++ Formula/chaosmonkey.rb
@@ -1,9 +1,9 @@
 class Chaosmonkey < Formula
   desc     "Controlled failure injection with Chaos Monkey"
   homepage "https://github.com/mlafeldt/chaosmonkey"
-  version  "0.5.2"
+  version  "0.5.3"
   url      "https://github.com/mlafeldt/chaosmonkey/releases/download/v#{version}/chaosmonkey_darwin_amd64"
-  sha256   "9823503ee3185221a623677cf0a7a6de4503aefcf7698436fb51052558a964ea"
+  sha256   "effe075bc49881453661d967f3497574f95a811f7b4b4f26ec76d73d876ec224"
 
   bottle :unneeded
 
